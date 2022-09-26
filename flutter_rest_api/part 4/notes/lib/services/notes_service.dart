56abb5068f7ed144dda19fc9b2f25f4de1f387d6
June Paolo Tabequero <tabequero.junepaolo@gmail.com>
2022-09-26 10:11:36 +0800

    exam reference

--- flutter_rest_api/part 4/notes/lib/services/notes_service.dart
+++ flutter_rest_api/part 4/notes/lib/services/notes_service.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
 
+import 'package:http/http.dart' as http;
 import 'package:notes/models/api_response.dart';
 import 'package:notes/models/note.dart';
 import 'package:notes/models/note_for_listing.dart';
-import 'package:http/http.dart' as http;
 
 class NotesService {
   static const API = 'http://api.notes.programmingaddict.com';
   static const headers = {'apiKey': '08d771e2-7c49-1789-0eaa-32aff09f1471'};
+
+  // reference
 
   Future<APIResponse<List<NoteForListing>>> getNotesList() {
     return http.get(API + '/notes', headers: headers).then((data) {
@@ -19,9 +21,10 @@ class NotesService {
         }
         return APIResponse<List<NoteForListing>>(data: notes);
       }
-      return APIResponse<List<NoteForListing>>(error: true, errorMessage: 'An error occured');
-    })
-    .catchError((_) => APIResponse<List<NoteForListing>>(error: true, errorMessage: 'An error occured'));
+      return APIResponse<List<NoteForListing>>(
+          error: true, errorMessage: 'An error occured');
+    }).catchError((_) => APIResponse<List<NoteForListing>>(
+        error: true, errorMessage: 'An error occured'));
   }
 
   Future<APIResponse<Note>> getNote(String noteID) {
@@ -31,7 +34,7 @@ class NotesService {
         return APIResponse<Note>(data: Note.fromJson(jsonData));
       }
       return APIResponse<Note>(error: true, errorMessage: 'An error occured');
-    })
-    .catchError((_) => APIResponse<Note>(error: true, errorMessage: 'An error occured'));
+    }).catchError((_) =>
+        APIResponse<Note>(error: true, errorMessage: 'An error occured'));
   }
 }
